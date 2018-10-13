37f4395742f09e99daf8ea2429e97c8cbc9aaa57
Alsha Samantaray <alsha1306.samantaray@gmail.com>
2018-10-13 10:49:29 -0700

    register user module db

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,25 @@
 const express = require ('express');
 const router = express.Router();
-
-
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const user = require('../models/users');
 //Register
-router.get('/register', (req, res, next) =>{
-    res.send('REGISTER');
+router.post('/register', (req, res, next) =>{
+    let newUser = new user({
+        name : req.body.name,
+        email : req.body.email,
+        username : req.body.username,
+        password : req.body.password
+    })
+
+    user.addUser(newUser,(err, usr) =>{
+        if(err){
+            res.json({success : false, msg : 'Failed to register user'});
+        }
+        else{
+            res.json({success : true, msg : 'User successfully registered'});
+        }
+    })
 });
 
 //Authenticate
